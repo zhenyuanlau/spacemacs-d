26c6e771c4465abff9ecf495755ff13b0bf6475e
Zhenyuan Lau <zhenyuanlau@icloud.com>
2018-08-21 12:10:03 +0800

    Add Chinese

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     chinese
      elixir
      (ruby :variables
            ruby-enable-enh-ruby-mode t
